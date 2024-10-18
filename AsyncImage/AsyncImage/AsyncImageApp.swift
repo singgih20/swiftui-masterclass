8ec26640339edce8f730f8597b119e6ec276eddb
Singgih <singgihaji@weekendinc.com>
2024-10-18 11:33:18 +0700

    section 3 async image

---
+++ AsyncImage/AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Singgih Aji Sasongko on 18/10/24.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
