c4f390f7dbb562720150283fef104fd61c543cd1
Singgih <singgihaji@weekendinc.com>
2024-10-18 20:44:21 +0700

    add reels and footer view

--- Slot Machine/Slot Machine/ContentView.swift
+++ Slot Machine/Slot Machine/ContentView.swift
@@ -44,9 +44,89 @@ struct ContentView: View {
                     .modifier(ScoreContainerModifier())
                 }
                 // MARK: - Slot Machine
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0) {
+                    // MARK: - Reel #1
+                    ZStack {
+                        ReelView()
+                        Image("gfx-bell")
+                            .resizable()
+                            .modifier(ImageModifier())
+                    }
+                    
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0){
+                        // MARK: - Reel #2
+                        ZStack {
+                            ReelView()
+                            Image("gfx-seven")
+                                .resizable()
+                                .modifier(ImageModifier())
+                        }
+                        
+                        Spacer()
+                        // MARK: - Reel #3
+                        
+                        ZStack {
+                            ReelView()
+                            Image("gfx-cherry")
+                                .resizable()
+                                .modifier(ImageModifier())
+                        }
+                    }
+                    .frame(maxWidth: 500)
+                    
+                    // MARK: - Spin Button
+                    Button(action: {
+                        print(1)
+                    }, label: {
+                        Image("gfx-spin")
+                            .renderingMode(.original)
+                            .resizable()
+                            .modifier(ImageModifier())
+                    })
+                }
+                .layoutPriority(2)
+                
                 // MARK: - Footer
                 
                 Spacer()
+                
+                HStack {
+                    // MARK: - Bet 20
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                        Button(action: {
+                            print("bet 20 coins")
+                        }, label: {
+                            Text("20")
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color.white)
+                                .modifier(BetNumberModifier())
+                        })
+                        .modifier(BetCapsuleModifier())
+                        
+                        Image("gfx-casino-chips")
+                            .resizable()
+                            .opacity(0)
+                            .modifier(CasinoChipsModifier())
+                    }
+                    //MARK: - Bet 10
+                    HStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 10) {
+                        Image("gfx-casino-chips")
+                            .resizable()
+                            .opacity(1)
+                            .modifier(CasinoChipsModifier())
+                        Button(action: {
+                            print("bet 10 coins")
+                        }, label: {
+                            Text("10")
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color.yellow)
+                                .modifier(BetNumberModifier())
+                        })
+                        .modifier(BetCapsuleModifier())
+                        
+                    }
+                }
+                
             }
             // MARK: - Buttons
             .overlay(
---
+++ Slot Machine/Slot Machine/ReelView.swift
@@ -0,0 +1,23 @@
+//
+//  ReelView.swift
+//  Slot Machine
+//
+//  Created by Singgih Aji Sasongko on 18/10/24.
+//
+
+import SwiftUI
+
+struct ReelView: View {
+    var body: some View {
+        Image("gfx-reel")
+            .resizable()
+            .modifier(ImageModifier())
+    }
+}
+
+struct ReelView_Previews: PreviewProvider {
+    static var previews: some View {
+        ReelView()
+            .previewLayout(.fixed(width: 220, height: 220))
+    }
+}
