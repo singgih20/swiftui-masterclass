4447c85eff35d0ae03dd57a1da03fbd6edfb5e5d
Singgih <singgihaji@weekendinc.com>
2024-10-29 11:08:07 +0700

    slicing home screen

--- Restart/Restart/Screens/HomeView.swift
+++ Restart/Restart/Screens/HomeView.swift
@@ -10,15 +10,40 @@ import SwiftUI
 struct HomeView: View {
     @AppStorage("onboarding") var isOnboardingViewActive: Bool = true
     var body: some View {
-        VStack {
-            Text("Homeview!")
-                .font(.largeTitle)
+        VStack(spacing: 20) {
+            // MARK: - HEADER
+            Spacer()
             
-            Button(action: {
-                isOnboardingViewActive = true
-            }, label: {
+            ZStack {
+                CircleGroupView(ShapeColor: .gray, ShapeOpacity: 0.1)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+                
+            // MARK: - CENTER
+            Text("The time that leads to mastery is dependent on the intensity of our focus.")
+                .font(.title3)
+                .fontWeight(.light)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+            // MARK: - FOOTER
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
+                    
                 Text("Restart")
-            })
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
+                    
+            }) //: BUTTON
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         } //: VStack
         
       
---
+++ Restart/Restart/Views/CircleGroupView.swift
@@ -0,0 +1,34 @@
+//
+//  CircleGroupView.swift
+//  Restart
+//
+//  Created by Singgih Aji Sasongko on 29/10/24.
+//
+
+import SwiftUI
+
+struct CircleGroupView: View {
+    
+    @State var ShapeColor: Color
+    @State var ShapeOpacity: Double
+    
+    var body: some View {
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 40)
+                .frame(width: 260, height: 260, alignment: .center)
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity), lineWidth: 80)
+                .frame(width: 260, height: 260, alignment: .center)
+        } //: ZSTACK
+    }
+}
+
+#Preview {
+    ZStack {
+        Color("ColorBlue")
+            .ignoresSafeArea(.all, edges: .all)
+        CircleGroupView(ShapeColor: .white, ShapeOpacity: 0.2)
+        
+    }
+}
