f9029081fd41cbaa7ebde3ba1702d4b4c641db2f
Singgih <singgihaji@weekendinc.com>
2024-10-31 18:53:26 +0700

    add control interface

---
+++ Pinch/Pinch/View/ControlImageView.swift
@@ -0,0 +1,24 @@
+//
+//  ControlImageView.swift
+//  Pinch
+//
+//  Created by Singgih Aji Sasongko on 31/10/24.
+//
+
+import SwiftUI
+
+struct ControlImageView: View {
+    let icon: String
+    
+    var body: some View {
+        Image(systemName: icon)
+            .font(.system(size: 36))
+    }
+}
+
+#Preview {
+    ControlImageView(icon: "minus.magnifyingglass")
+        .preferredColorScheme(.dark)
+        .previewLayout(.sizeThatFits)
+        .padding()
+}
