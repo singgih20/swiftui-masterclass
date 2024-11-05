92f2a96cd5e0166b5de5afe4e1580fa07c77d960
Singgih <singgihaji@weekendinc.com>
2024-11-05 10:39:18 +0700

    create listView

---
+++ Fructus/Fructus/App/FruitDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  FruitDetailView.swift
+//  Fructus
+//
+//  Created by Singgih Aji Sasongko on 05/11/24.
+//
+
+import SwiftUI
+
+struct FruitDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FruitDetailView()
+}
---
+++ Fructus/Fructus/View/FruitRowView.swift
@@ -0,0 +1,45 @@
+//
+//  FruitRowView.swift
+//  Fructus
+//
+//  Created by Singgih Aji Sasongko on 05/11/24.
+//
+
+import SwiftUI
+
+struct FruitRowView: View {
+    // MARK: - PROPERTIES
+    var fruit: Fruit
+    // MARK: - BODY
+    var body: some View {
+        HStack {
+            Image(fruit.image)
+                .renderingMode(.original)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80, alignment: .center)
+                .shadow(color: .black.opacity(0.3), radius: 3, x: 2, y: 2)
+                .background(
+                    LinearGradient(gradient: Gradient(colors: fruit.gradientColors), startPoint: .top, endPoint: .bottom)
+                )
+                .cornerRadius(8)
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text(fruit.title)
+                    .font(.title2)
+                    .fontWeight(.bold)
+                Text(fruit.headline)
+                    .font(.caption)
+                    .foregroundColor(Color.secondary)
+            }
+        } //: HStack
+    }
+}
+
+struct FruitRowView_Preview: PreviewProvider {
+    static var previews: some View {
+        FruitRowView(fruit: fruitsData[0])
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
