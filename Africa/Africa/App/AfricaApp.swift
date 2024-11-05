8b15adf71d7f3bcca91deb3f4a83a704c4cff27e
Singgih <singgihaji@weekendinc.com>
2024-11-05 18:53:33 +0700

    setup project

---
+++ Africa/Africa/App/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Singgih Aji Sasongko on 05/11/24.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
