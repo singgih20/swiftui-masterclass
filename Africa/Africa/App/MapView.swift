c0329359e528c94eadea6a41e8b69607a4f4d63f
Singgih <singgihaji@weekendinc.com>
2024-11-06 17:47:39 +0700

    finish map screen

--- Africa/Africa/App/MapView.swift
+++ Africa/Africa/App/MapView.swift
@@ -21,22 +21,55 @@ struct MapView: View {
     let locations: [NationalParkLocation] = Bundle.main.decode("locations.json")
     
     var body: some View {
-//        Map(coordinateRegion: $region)
+        //        Map(coordinateRegion: $region)
         
         Map(coordinateRegion: $region, annotationItems: locations, annotationContent: { item in
-//            old style(always static)
-//            MapPin(coordinate: item.location, tint: .accentColor)
-
-//            New style (always static)
-//            MapMarker(coordinate: item.location, tint: .accentColor)
-            
             MapAnnotation(coordinate: item.location) {
-                Image("map-solonga")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 32, height: 32, alignment: .center)
+                MapAnnotationView(location: item)
             }
         })
+        .overlay(
+            HStack(alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48, alignment: .center)
+                
+                VStack(alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    Divider()
+                    HStack {
+                        Text("Longitude:")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+                
+            }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(
+                    Color.black
+                        .cornerRadius(8)
+                        .opacity(0.6)
+                )
+                .padding()
+            , alignment: .top
+        )
     }
 }
 
