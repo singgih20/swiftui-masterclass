1eb2dd988f68b9b0da75eae7cbe8da402d77314a
Singgih <singgihaji@weekendinc.com>
2024-11-06 16:30:02 +0700

    finish video preview

--- Africa/Africa/App/ContentView.swift
+++ Africa/Africa/App/ContentView.swift
@@ -10,14 +10,12 @@ import SwiftUI
 struct ContentView: View {
     
     let animals: [Animal] = Bundle.main.decode("animals.json")
-
     var body: some View {
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
                 ForEach(animals) { item in
                     NavigationLink(destination: AnimalDetailView(animal: item)) {
                         AnimalListItemView(animal: item)
